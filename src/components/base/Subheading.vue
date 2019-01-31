75023f5f2b8873d52ec1eda050a27384b494df36
John Leider <john.j.leider@gmail.com>
2019-01-31 14:18:52 -0600

    feat(home/About): add new component

---
+++ src/components/base/Subheading.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2
+    class="subheading text-uppercase font-weight-medium"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </h2>
+</template>
+
+<script>
+  export default {
+    name: 'BaseSubheading',
+
+    inheritAttrs: false
+  }
+</script>
--- src/plugins/base.js
+++ src/plugins/base.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import BaseCard from '@/components/base/Card'
+import BaseSubheading from '@/components/base/Subheading'
 
 Vue.component(BaseCard.name, BaseCard)
+Vue.component(BaseSubheading.name, BaseSubheading)
